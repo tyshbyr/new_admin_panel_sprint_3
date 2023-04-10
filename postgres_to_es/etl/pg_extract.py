878415d1417bb11a8aec5b19aa781f994a35cdfd
tyshbyr <tyshbyr@gmail.com>
2023-04-10 19:00:57 +0500

    Оптимизирован SQL запрос

--- postgres_to_es/etl/pg_extract.py
+++ postgres_to_es/etl/pg_extract.py
@@ -87,16 +87,16 @@ class PostgresExtractor:
                                     fw.type, 
                                     fw.created, 
                                     fw.modified, 
-                                    pfw.role, 
-                                    p.id as p_id, 
-                                    p.full_name,
-                                    g.name
+                                    STRING_AGG(DISTINCT pfw.role, ',') as roles, 
+                                    STRING_AGG(DISTINCT CONCAT(p.id, ':', p.full_name), ',') as persons,
+                                    STRING_AGG(DISTINCT g.name, ',') as genres
                                 FROM content.film_work fw
                                 LEFT JOIN content.person_film_work pfw ON pfw.film_work_id = fw.id
                                 LEFT JOIN content.person p ON p.id = pfw.person_id
                                 LEFT JOIN content.genre_film_work gfw ON gfw.film_work_id = fw.id
                                 LEFT JOIN content.genre g ON g.id = gfw.genre_id
-                                WHERE fw.id IN %s; 
+                                WHERE fw.id IN %s
+                                GROUP BY fw.id, fw.title, fw.description, fw.rating, fw.type, fw.created, fw.modified 
                             """, (batch_ids,))
                     batch_movies = tuple(dict(row) for row in curs.fetchall())
                     yield batch_movies
