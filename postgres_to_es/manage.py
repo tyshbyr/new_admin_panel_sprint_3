134080f0e72d6736bba942c24012f13c863bc851
tyshbyr <tyshbyr@gmail.com>
2023-04-12 17:34:02 +0500

    Добавлен manage.py

--- postgres_to_es/manage.py
+++ postgres_to_es/manage.py
@@ -1,20 +1,19 @@
-"""
-    Запускает ETL процесс
-    Управляет загрузкой данных из Postgres
-    Управляет трансформацией данных
-    Управляет загрузкой данных в ES
-    Управляет сохранением состояния 
-"""
-
-
 from settings import dsl, EXTRACT_BATCH_SIZE
-from etl.pg_extract import extract_updated_movies
-from datetime import datetime
+from etl.pg_extract import PostgresExtractor
+import psycopg2
+from etl.state_etl import JsonFileStorage, State
+import psycopg2.extras
+from etl.es_load import ElasticsearchLoader
 
 
-extract_batch = EXTRACT_BATCH_SIZE
-dt = datetime.fromisoformat('2021-06-15 20:14:09.514476+00')
+batch_size = EXTRACT_BATCH_SIZE
+storage = JsonFileStorage('storage.txt')
+state = State(storage)
+loader = ElasticsearchLoader('movies', es_host='127.0.0.1', es_port=9200)
 
-movies = extract_updated_movies(dsl, extract_batch, dt)
-print(next(movies)[0])
-print(next(movies)[0])
+with psycopg2.connect(**dsl) as conn:
+        extract = PostgresExtractor(conn, batch_size, state)
+        batch_movies_generator = extract.get_updated_movies()
+        movies = []
+        for batch in batch_movies_generator:
+            loader.load_movies(batch, batch_size)
