ab83465e0167618e761c9f9dc0bc2842bec5c3ef
tyshbyr <tyshbyr@gmail.com>
2023-04-12 17:19:12 +0500

    Добавлен Transform

---
+++ postgres_to_es/etl/transform.py
@@ -0,0 +1,60 @@
+from dataclasses import dataclass
+from typing import List
+from uuid import UUID
+
+@dataclass
+class Person:
+    id: UUID
+    name: str
+    
+    def as_dict(self):
+        return {'id': str(self.id), 'name': self.name}
+
+
+@dataclass
+class Movie:
+    #_id: UUID
+    id: UUID
+    imdb_rating: float
+    genre: List[str]
+    title: str
+    description: str
+    director: List[str]
+    actors: List[Person]
+    writers: List[Person]
+    actors_names: List[str]
+    writers_names: List[str]
+
+    def __init__(self, row):
+        #self._id = UUID(row['id'])
+        self.id = UUID(row['id'])
+        self.imdb_rating = row['imdb_rating']
+        self.genre = row['genre']
+        self.title = row['title']
+        self.description = row['description']
+        self.director = row['director'] if row['director'] else ''
+        if row['actors'] is not None:
+            self.actors = [Person(UUID(actor['id']), actor['name']) for actor in row['actors']]
+        else:
+            self.actors = []
+        if row['writers'] is not None:
+            self.writers = [Person(UUID(writer['id']), writer['name']) for writer in row['writers']]
+        else:
+            self.writers = []
+        self.actors_names = row['actors_names']
+        self.writers_names = row['writers_names']
+        
+    def as_dict(self):
+        return {
+            #'_id': str(self._id),
+            'id': str(self.id),
+            'imdb_rating': self.imdb_rating,
+            'genre': self.genre,
+            'title': self.title,
+            'description': self.description,
+            'director': self.director,
+            'actors': [actor.as_dict() for actor in self.actors],
+            'writers': [writer.as_dict() for writer in self.writers],
+            'actors_names': self.actors_names,
+            'writers_names': self.writers_names
+        }
